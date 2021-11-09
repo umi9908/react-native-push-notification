6d156e030a423503b2b7d2d532c0bad7abbc48a7
asamisawai <asami.umibayashi@comnet-corp.co.jp>
2021-11-09 16:14:36 +0900

    expo-permissionが非推奨になるため、nottificationにコードを書き換え
    packageからもexpo-permissionを削除

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-import * as Permissions from 'expo-permissions';
-import * as Notifications from 'expo-notifications';
-import NCMB, { NCMBInstallation } from 'ncmb-react-native';
-import Constants from 'expo-constants';
-import { Platform } from 'react-native';
+import * as Notifications from "expo-notifications";
+import NCMB, { NCMBInstallation } from "ncmb-react-native";
+import Constants from "expo-constants";
+import { Platform } from "react-native";
 
 class NCMBPushNotification {
   private installation: NCMBInstallation | null = null;
@@ -15,21 +14,21 @@ class NCMBPushNotification {
     const token = await this.getDeviceToken();
     this.installation = new NCMBInstallation();
     this.installation
-      .set('deviceToken', token)
-      .set('deviceType', Platform.OS === 'ios' ? 'ios' : 'android');
+      .set("deviceToken", token)
+      .set("deviceType", Platform.OS === "ios" ? "ios" : "android");
     console.log(this.installation);
     await this.installation.save();
     return true;
   }
 
   async getDeviceToken(): Promise<string | null> {
-    if (!Constants.isDevice) throw new Error('This is not device');
-    let { status: currentStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-    if (currentStatus !== 'granted') {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (!Constants.isDevice) throw new Error("This is not device");
+    let { status: currentStatus } = await Notifications.getPermissionsAsync();
+    if (currentStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
       currentStatus = status;
     }
-    if (currentStatus !== 'granted') {
+    if (currentStatus !== "granted") {
       return null;
     }
     return (await Notifications.getDevicePushTokenAsync()).data as string;
@@ -40,9 +39,7 @@ class NCMBPushNotification {
     const token = await this.getDeviceToken();
     if (token === null) return null;
     const query = NCMBInstallation.query();
-    this.installation = await query
-      .equalTo('deviceToken', token)
-      .fetch();
+    this.installation = await query.equalTo("deviceToken", token).fetch();
     return this.installation;
   }
 }
